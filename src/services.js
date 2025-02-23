522b5a4f359108d0cff8f7db174dee34659f5a91
diegodistefano <diego.distefano.1989@gmail.com>
2025-02-23 16:10:24 +0100

    edit listo

--- src/services.js
+++ src/services.js
@@ -12,7 +12,7 @@ async function getAllFilms() {
     catch {
         console.log("Error");
     }
-}    
+}
 
 // PRINT
 function printFilms(data) {
@@ -20,7 +20,7 @@ function printFilms(data) {
     data.forEach(film => {
         const tr = document.createElement('tr');
         tr.id = `row-${String(film.id)}`;
-        
+
         tr.innerHTML = `
             <td>${film.title}</td>
             <td>${film.year}</td>
@@ -49,7 +49,7 @@ async function createFilms() {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ title, year, director })
         });
-        
+
         const newFilm = await response.json();
         addFilmToTable(newFilm);
         formulario.reset();
@@ -63,77 +63,77 @@ function addFilmToTable(film) {
     const tr = document.createElement("tr");
     tr.id = `${String(film.id)}`;
     tr.innerHTML = `
-
         <td>${film.title}</td>
         <td>${film.year}</td>
         <td>${film.director}</td>
         <td><button class="edit-btn" onclick="updateFilm(${String(film.id)}, '${film.title}', '${film.year}', '${film.director}')">Editar</button></td>
         <td><button class="delete-btn" onclick="deleteFilm(${String(film.id)})">Eliminar</button></td>
-    `;
+        `;
     table.appendChild(tr);
+}
+
+// DELETE: method DELETE
+async function deleteFilm(id) {
+    if (!confirm("¿Desea confirmar la eliminación?")) return;
+
+    try {
+        await fetch(`${URL}/${id}`,
+            {
+                method: "DELETE"
+            });
+        document.getElementById(`row-${id}`).remove();
+    } catch (error) {
+        console.error("Error al eliminar la película:", error);
+    }
+}
+
+
+// UPDATE: method PUT
+async function updateFilm(id, oldTitle, oldYear, oldDirector) {
+    id = String(id);
+
+    
+    const newTitle = prompt("Nuevo título:", oldTitle)?.trim();
+    const newYear = prompt("Nuevo año:", oldYear)?.trim();
+    const newDirector = prompt("Nuevo director:", oldDirector)?.trim();
+
+    if (!newTitle || !newYear || !newDirector) {
+        console.log("Datos obligatorios");
+        return;
+    }
+
+    try {
+        let newId = id.replace("-", "");
+        await fetch(`${URL}/${newId}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                title: newTitle,
+                year: newYear,
+                director: newDirector
+            })
+        });
+        updateFilmInTable(newId, newTitle, newYear, newDirector);
+        getAllFilms();
+    } catch (error) {
+        console.error("Error al actualizar la película:", error);
+    }
 }
 
 
 
-
-// // UPDATE: method PUT
-// async function updateFilm(id, oldTitle, oldYear, oldDirector) {
-//     id = String(id);
-//     const newTitle = prompt("Nuevo título:", oldTitle)?.trim();
-//     const newYear = prompt("Nuevo año:", oldYear)?.trim();
-//     const newDirector = prompt("Nuevo director:", oldDirector)?.trim();
-
-//     if (!newTitle || !newYear || !newDirector) {
-//         console.log("Datos obligatorios");
-//         return;
-//     }
-
-//     try {
-//         await fetch(`${URL}/${id}`, {
-//             method: "PUT",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify({
-//                 title: newTitle,
-//                 year: newYear,
-//                 director: newDirector
-//             })
-//         });
-//         updateFilmInTable(id, newTitle, newYear, newDirector);
-//     } catch (error) {
-//         console.error("Error al actualizar la película:", error);
-//     }
-// }
-
-// // DELETE: method DELETE
-// async function deleteFilm(id) {
-//     if (!confirm("¿Desea confirmar la eliminación?")) return;
-
-//     try {
-//         await fetch(`${URL}/${id}`,
-//             {
-//                 method: "DELETE"
-//             });
-//             document.getElementById(`row-${id}`).remove();
-//     } catch (error) {
-//         console.error("Error al eliminar la película:", error);
-//     }
-// }
-
-
-// function updateFilmInTable(id, title, year, director) {
-//     id = String(id);
-//     const row = document.getElementById(`row-${id}`);
-//     if (row) {
-//         row.innerHTML = `
-//             <td>${id}</td>
-//             <td>${title}</td>
-//             <td>${year}</td>
-//             <td>${director}</td>
-//             <td><button class="edit-btn" onclick="updateFilm(${String(id)}, '${title}', '${year}', '${director}')">Editar</button></td>
-//             <td><button class="delete-btn" onclick="deleteFilm(${String(id)})">Eliminar</button></td>
-//         `;
-//     }
-// }
+function updateFilmInTable(id, title, year, director) {
+    id = String(id);
+    const row = document.getElementById(`row-${String(id)}`);
+    if (row) {
+        row.innerHTML = `
+            <td>${title}</td>
+            <td>${year}</td>
+            <td>${director}</td>
+            <td><button class="edit-btn" onclick="updateFilm(${String(id)}, '${title}', '${year}', '${director}')">Editar</button></td>
+            <td><button class="delete-btn" onclick="deleteFilm(${String(id)})">Eliminar</button></td>
+        `;
+    }
+}
 
 getAllFilms();
-
